193083140344b0b678cc4943f1cf4caa4722ffa6
Konstantin <Super.Juke88@yandex.ru>
2022-01-21 02:09:26 +0400

    add: login, catalog and distribution page; create: accordion js, login js

--- build/js/main.js
+++ build/js/main.js
@@ -6,11 +6,12 @@
   const body = document.querySelector('.page__body');
   const header = document.querySelector('.header');
   const intro = header.querySelector('.header__intro');
-  const btnMenu = header.querySelector('.header__nav-btn');
+  const btnMenu = header.querySelector('.button');
   const headerWrap = header.querySelector('.header__wrapper');
   const menu = header.querySelector('.header__nav-menu');
   const overlay = document.querySelector('.overlay');
   const accordion = document.querySelector('.accordion');
+  const login = menu.querySelector('#login')
 
   headerWrap.classList.remove('header__wrapper--no-js');
   menu.classList.remove('header__nav-menu--no-js');
@@ -28,7 +29,7 @@
   });
 
   btnMenu.addEventListener('click', () => {
-    if (btnMenu.classList.contains('header__nav-btn--burger')) {
+    if (btnMenu.classList.contains('button--burger')) {
       showMenu(btnMenu, headerWrap, menu);
       setOverlayVisible(overlay);
     } else {
@@ -48,101 +49,58 @@
   }
 
   function showMenu(btn, wrapper, menu) {
-    btn.classList.remove('header__nav-btn--burger');
-    btn.classList.add('header__nav-btn--closed');
+    btn.classList.remove('button--burger');
+    btn.classList.add('button--closed');
     wrapper.classList.add('header__wrapper--open-menu');
     menu.classList.add('header__nav-menu--show');
   }
 
   function hideMenu(btn, wrapper, menu) {
-    btn.classList.add('header__nav-btn--burger');
-    btn.classList.remove('header__nav-btn--closed');
+    btn.classList.add('button--burger');
+    btn.classList.remove('button--closed');
     wrapper.classList.remove('header__wrapper--open-menu');
     menu.classList.remove('header__nav-menu--show');
   }
 
   // POPUP
 
-  // const formTemplate = document.querySelector('#popup').content.querySelector('.modal');
-  // const btnCallback = document.querySelector('.button--callback');
-  // const overlay = document.querySelector('.overlay');
+  const formTemplate = document.querySelector('#popup').content.querySelector('.modal-login');
+  const btnLogin = document.querySelector('.header__login');
 
-  // const isEscEvent = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
+  const closePopup = (popup) => {
+    popup.remove();
+    setOverlayHide(overlay);
+  }
 
-  // const closePopup = (popup) => {
-  //   popup.remove();
-  //   document.body.style.overflow = '';
-  //   overlay.classList.add('overlay--hidden');
-  // }
+  const showFormLogin = (template) => {
+    const btnFormClose = template.querySelector('.modal-login__button');
 
-  // const showFormCallback = (formTemplate) => {
-  //   const btnFormClose = formTemplate.querySelector('.modal__btn');
-  //   const inputPhoneForm = formTemplate.querySelector('[name="modal-phone"]');
-  //   const btnFormSubmit = formTemplate.querySelector('.button--modal');
+    document.body.insertAdjacentElement('beforeend', template);
+    setOverlayVisible(overlay);
 
-  //   document.body.insertAdjacentElement('beforeend', formTemplate);
-  //   document.body.style.overflow = 'hidden';
+    btnFormClose.addEventListener('click', () => closePopup(template), {once: true});
+    overlay.addEventListener('click', () => closePopup(template), {once: true});
 
-  //   overlay.classList.remove('overlay--hidden');
+    document.addEventListener('keydown', (evt) => {
+      if (isEscEvent(evt)) {
+        evt.preventDefault();
+        closePopup(template);
+      }
+    }, {once: true});
+  };
 
-  //   let name = formTemplate.querySelector('#modal-name-id');
-  //   let phone = formTemplate.querySelector('#modal-phone-id');
-  //   let message = formTemplate.querySelector('#modal-question-id');
-  //   name.focus();
+  const showPopup = (evt) => {
+    evt.preventDefault();
+    showFormLogin(formTemplate.cloneNode(true));
+  }
 
-  //   let isStorageSupport = true;
-  //   let storageName = '';
-  //   let storagePhone = '';
-  //   let storageMessage = '';
+  btnLogin.addEventListener('click', showPopup);
 
-  //   try {
-  //     storageName = localStorage.getItem('name');
-  //     storagePhone = localStorage.getItem('phone');
-  //     storageMessage = localStorage.getItem('message');
-  //   } catch (err) {
-  //     isStorageSupport = false;
-  //   }
-
-  //   if (storageName) {
-  //     name.value = storageName;
-  //     phone.focus();
-  //   }
-  //   if (storagePhone) {
-  //     phone.value = storagePhone;
-  //     message.focus();
-  //   }
-  //   if (storageMessage) {
-  //     message.innerText = storageMessage;
-  //   } else {
-  //     name.focus();
-  //   }
-
-  //   btnFormSubmit.addEventListener('submit', function (evt) {
-  //     if (!name.value || !phone.value || !message.value) {
-  //       evt.preventDefault();
-  //     } else {
-  //       if (isStorageSupport) {
-  //         localStorage.setItem('name', name.value);
-  //         localStorage.setItem('phone', phone.value);
-  //         localStorage.setItem('message', message.value);
-  //       }
-  //     }
-  //   });
-  //   btnFormClose.addEventListener('click', () => closePopup(formTemplate), {once: true});
-  //   overlay.addEventListener('click', () => closePopup(formTemplate), {once: true});
-
-  //   validatePhone(inputPhoneForm);
-
-  //   document.addEventListener('keydown', (evt) => {
-  //     if (isEscEvent(evt)) {
-  //       evt.preventDefault();
-  //       closePopup(formTemplate);
-  //     }
-  //   }, {once: true});
-  // };
-
-  // // const showPopup = () => showFormCallback(formTemplate.cloneNode(true));
-  // // btnCallback.addEventListener('click', showPopup);
+  login.addEventListener('click', (evt) => {
+    setOverlayVisible(overlay);
+    hideMenu(btnMenu, headerWrap, menu);
+    showPopup(evt);
+  });
 
   // = = = = = = = = = = = = = = = = = = = = = = = = = = = //
 
